72b2d81bb24fb856bf7253b1604cdf3e911d6f57
lamlv <lam.luongvan2305@gmail.com>
2020-07-31 14:28:35 +0700

    add script to install dev dependencies

---
+++ template/install-dev-dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+npm list @types/react || npm install --save-dev @types/react@16.9.38
+
+npm list @types/react-redux || npm install --save-dev @types/react-redux@7.1.9
+
+npm list @types/react-dom || npm install --save-dev @types/react-dom@16.9.8
+
+npm list @types/react-router-dom || npm install --save-dev @types/react-router-dom@5.1.5
